f5498a7d51c4292f89e29bd29378012a902e3ad6
Dominika Ejsmont <d.ejsmont95@gmail.com>
2023-09-08 20:26:04 +0200

    Add DTO files for products and orders, add validation to endpoints, add /api to path

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { LoggerInterceptor } from './shared/interceptors/logger.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new LoggerInterceptor());
+  app.setGlobalPrefix('api');
+  await app.listen(8000);
 }
 bootstrap();
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  NotFoundException,
+  Post,
+  Put,
+  Delete,
+  Body,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { CreateOrderDTO } from './dtos/create-order-dto';
+import { UpdateOrderDTO } from './dtos/update-order-dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -11,7 +23,34 @@ export class OrdersController {
   }
 
   @Get('/:id')
-  public getById(@Param('id') id: string) {
-    return this.ordersService.getById(id);
+  getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const prod = this.ordersService.getById(id);
+    if (!prod) throw new NotFoundException('Product not found');
+    return prod;
+  }
+
+  @Delete('/:id')
+  deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!this.ordersService.getById(id))
+      throw new NotFoundException('Product not found');
+    this.ordersService.deleteById(id);
+    return { success: true };
+  }
+
+  @Post('/')
+  create(@Body() orderData: CreateOrderDTO) {
+    return this.ordersService.create(orderData);
+  }
+
+  @Put('/:id')
+  update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() orderData: UpdateOrderDTO,
+  ) {
+    if (!this.ordersService.getById(id))
+      throw new NotFoundException('Product not found');
+
+    this.ordersService.updateById(id, orderData);
+    return { success: true };
   }
 }
